e74c304e42215fe3cae8d695b4c836ae54e769ef
yossizap <yossizap@gmail.com>
2016-11-26 22:40:34 +0200

    Added externs to the func wrapper and an option to draw a closing line

--- spherical_laser_projector/svg2c.py
+++ spherical_laser_projector/svg2c.py
@@ -18,15 +18,23 @@ ARC_FUNC_NAME = "draw_arc"
 TURN_OFF_LASER_FUNC_NAME = "set_laser"
 
 
-def generate_code(paths):
+def generate_code(paths, close=True):
     '''
     Return a string that contains the generated C code from the given paths list
     '''
     gen_code = ""
+    instructions_counter = 0
     # Go over all the paths in the paths list, parse them as svg.path classes and convert them to
     # function calls
     for path in paths:
         for instruction in svg.path.parse_path(path):
+            # Log the first instruction's location to return to it when "close" is True. 
+            # NOTE: It might wiser to place this for the first path and do the closing on the last path. Requires testing.
+            instructions_counter += 1
+            if 1 == instructions_counter:
+                start_x = instruction.start.real
+                start_y = instruction.start.real
+                
             if isinstance(instruction, svg.path.QuadraticBezier):
                 gen_code += create_function_call(QUADRATIC_BEZIER_FUNC_NAME,
                         "{XSTART}, {YSTART}, {XCNT}, {YCNT}, {XEND}, {YEND}".format(
@@ -37,7 +45,7 @@ def generate_code(paths):
                             XEND=round_float(instruction.end.real), 
                             YEND=round_float(instruction.end.imag),)
                         )
-            if isinstance(instruction, svg.path.Line):
+            elif isinstance(instruction, svg.path.Line):
                 gen_code += create_function_call(LINE_FUNC_NAME, 
                         "{XSTART}, {YSTART}, {XEND}, {YEND}".format(
                             XSTART=round_float(instruction.start.real), 
@@ -45,7 +53,7 @@ def generate_code(paths):
                             XEND=round_float(instruction.end.real), 
                             YEND=round_float(instruction.end.imag),)
                         )
-            if isinstance(instruction, svg.path.CubicBezier):
+            elif isinstance(instruction, svg.path.CubicBezier):
                 gen_code += create_function_call(CUBIC_BEZIER_FUNC_NAME,
                         "{XSTART}, {YSTART}, {XCNT1}, {YCNT1}, {XCNT2}, {YCNT2}, {XEND}, {YEND}".format(
                             XSTART=round_float(instruction.start.real), 
@@ -57,7 +65,7 @@ def generate_code(paths):
                             XEND=round_float(instruction.end.real), 
                             YEND=round_float(instruction.end.imag),)
                         )
-            if isinstance(instruction, svg.path.Arc):
+            elif isinstance(instruction, svg.path.Arc):
                 gen_code += create_function_call(ARC_FUNC_NAME, 
                     "{XSTART}, {YSTART}, {XRADIUS}, {YRADIUS}, {ROTATION}, {ARC}, {SWEEP}, {XEND}, {YEND}".format(
                             XSTART=round_float(instruction.start.real), 
@@ -70,17 +78,26 @@ def generate_code(paths):
                             XEND=round_float(instruction.end.real), 
                             YEND=round_float(instruction.end.imag),)
                         )
-
+                        
+        # If close is enabled we have to draw a line back to the starting position from this instruction's end position
+        if True == close:
+            gen_code += create_function_call(LINE_FUNC_NAME,
+                "{XSTART}, {YSTART}, {XEND}, {YEND}".format(
+                    XSTART=round_float(instruction.start.real), 
+                    YSTART=round_float(instruction.start.imag),
+                    XEND=round_float(start_x), 
+                    YEND=round_float(start_y),)
+                )
+            
         # Turn off the laser once a path is finished
         gen_code += create_function_call(TURN_OFF_LASER_FUNC_NAME, "false")
-
+        
     return gen_code
 
 
 def round_float(number):
     return int(round(number * ROUNDING_MULTIPLIER))
 
-  
 
 def create_function_call(func_name, arguments):
     '''
@@ -110,18 +127,24 @@ def create_function_wrapper(func_name, func_content):
     '''
     indent = "\t"
     indented_code = ''.join(indent+line for line in func_content.splitlines(True))
-    return "void %s()\n{\n%s}" % (func_name, indented_code)
+    wrapper = "extern void set_laser(bool is_on);\n"
+    wrapper += "extern void draw_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1);\n"
+    wrapper += "extern void draw_quadratic_bezier(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);\n"
+    wrapper += "extern void draw_cubic_bezier(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3);\n"
+    wrapper += "void %s()\n{\n%s}" % (func_name, indented_code)
+    return wrapper
 
 
 if "__main__" == __name__:
     parser = OptionParser()
     parser.add_option("-i", "--input_file", dest="input_file", help="SVG input file")
     parser.add_option("-o", "--output_file", dest="output_file", help="Generated code output file path")
-
+    parser.add_option("-c", "--close_svg", dest="close", action="store_false", default=True, help="Whether the script should add a line to the starting point when reaching the 'z' instruction(Default=True)")
+    
     (options, args) = parser.parse_args()
-
+    
     paths = parse_paths_from_svg(options.input_file)
-    code = generate_code(paths)
+    code = generate_code(paths, options.close)
 
     with open(options.output_file, "wb") as f:
         f.write(create_function_wrapper("draw_" +
